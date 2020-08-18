98006eab7ffc8a4b9ac16363e72962211a345fbb
Morgan Belous <morganbelous@gmail.com>
2020-08-17 23:16:00 -0400

    app setup

---
+++ Victor/Victor/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewController.swift
+//  Victor
+//
+//  Created by Morgan Belous on 8/17/20.
+//  Copyright © 2020 Morgan Belous. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+    }
+    
+
+
+
+}
